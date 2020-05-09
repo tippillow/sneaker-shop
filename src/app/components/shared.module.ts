275e1c4a37358f838b1b7f92cbeee1b13a4b49fc
tippillow <artemnazarikov@gmail.com>
2020-05-09 21:41:51 +0200

    fix in shared module

--- src/app/components/shared.module.ts
+++ src/app/components/shared.module.ts
@@ -27,7 +27,9 @@ const SHARED_COMPONENTS = [
     CustomSizesComponent,
     ShoeItemComponent,
     NotificationComponent,
-    CustomSelectComponent
+    CustomSelectComponent,
+    PromoItemComponent,
+    CutPipe
 ];
 
 @NgModule({
